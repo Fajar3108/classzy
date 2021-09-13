e613f7bb7ce14abb7a3e92b1fa02a1421bd378c8
Fajar3108 <maulanafajaribrahim@gmail.com>
2021-09-13 17:30:39 +0700

    first commit

---
+++ database/migrations/2021_09_13_102633_create_classes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('teacher_id');
+            $table->foreignId('vocation_id')->constrained();
+            $table->foreignId('grade_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+}
